80d19583dbeda5fd5a0a012bc7b628c8fd85b61f
Abhishek Santra <santrabhishek@gmail.com>
2022-10-29 12:52:51 +0530

    ShoppingList using React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import './App.css'
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId]=useState(0);
+  const handlesubmit=(e)=>{
+    e.preventDefault();
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id
+          ? (t = { id: t.id, todo })
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+    if (todo !==''){
+      setTodos([{id:`${todo}-${Date.now()}`,todo}, ...todos]);
+      setTodo("");
+    }
 
-function App() {
+  };
+  const handleDelete=(id)=>{
+    const delTodo=todos.filter((to)=>to.id!==id);
+    setTodos([...delTodo]);
+
+  }
+  const handleCheck = event=> {
+    event.currentTarget.style.color = 'green';
+    
+  };
+  const handleEdit=(id)=>{
+    const editTodo=todos.find((i)=>i.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='container'>
+         <h1>Shopping Master</h1>
+         <hr/>
+         <form className='inputBox' onSubmit={handlesubmit}>
+            <input type="text" value={todo} placeholder='Add Items...'onChange={(e)=>setTodo(e.target.value)}/>
+            <button type='submit'>{editId?"Edit":"Add"}</button>
+         </form>
+         <ul className='alltools'>
+          {
+            todos.map((t)=>(              
+          <li className='singletool'key={t.id}>
+            <span className='text'key={t.id}>{t.todo}</span>
+            <i className='fas fa-check-square' onClick={handleCheck}></i>
+            <i className='fas fa-trash'onClick={()=>handleDelete(t.id)}></i>
+            <i className='fas fa-edit'onClick={()=>handleEdit(t.id)}></i>
+          </li>
+            ))
+          }
+          
+         </ul>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
